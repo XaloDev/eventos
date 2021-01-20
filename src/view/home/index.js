86b7775393c76696f6333a6af3b3f917434eaae4
Ícaro Gabriel <icarogabriel999@gmail.com>
2021-01-20 03:40:27 -0300

    Alpha version, falta edição e remoção.

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,17 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
 import {Link} from 'react-router-dom';
 import Navbar from '../../components/navbar';
 import { useSelector } from 'react-redux';
+import firebase from '../../config/firebase'
+import EventoCard from '../../components/evento-card';
 
 
-function Home(){
+
+function Home({match}){
+    
+    const [eventos, setEventos] = useState([]);
+    const [pesquisa, setPesquisa] = useState('');
+
+    let listaeventos = [];
+    const usuarioEmail = useSelector(state => state.usuarioEmail);
+
+    useEffect(() => {
+
+        if(match.params.parametro){
+            firebase.firestore().collection('eventos').where('usuario','==',usuarioEmail).get().then(async (resultado) => {
+                await resultado.docs.forEach(doc => {
+                    if(doc.data().titulo.indexOf(pesquisa) >= 0)
+                    {
+                    listaeventos.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+                    }
+                })
+    
+                setEventos(listaeventos);
+    
+            })
+        }else{
+            firebase.firestore().collection('eventos').get().then(async (resultado) => {
+                await resultado.docs.forEach(doc => {
+                    if(doc.data().titulo.indexOf(pesquisa) >= 0)
+                    {
+                    listaeventos.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+                    }
+                })
+    
+                setEventos(listaeventos);
+    
+            })
+        }
+
+        
+    });
+
+    
     return(
         <>
             <Navbar/>
-            <h1>{useSelector(state => state.usuarioEmail)}</h1>
-            <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
 
+            <div className="row p-5 text-center">
+                <h2 className="mx-auto p-5">Eventos Publicados</h2>
+                <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center" placeholder="Pesquisar Evento pelo título..." />
+            </div>
+
+            <div className="row  p-3">
+                {eventos.map(item => <EventoCard id={item.id} img={item.foto}
+                titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes}
+                />) }   
+            </div>
         </>
     )
 }
