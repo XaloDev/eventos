959f0962cefaa30999b82547412903eda93be528
Ícaro Gabriel <icarogabriel999@gmail.com>
2021-01-21 11:57:00 -0300

    Refine

--- src/view/evento-cadastro/index.js
+++ src/view/evento-cadastro/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 import './evento-cadastro.css';
-import {Link} from 'react-router-dom';
 
 import Navbar from '../../components/navbar';
 import firebase from '../../config/firebase';
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './home.css';
-import {Link} from 'react-router-dom';
+
 import Navbar from '../../components/navbar';
 import { useSelector } from 'react-redux';
 import firebase from '../../config/firebase'
